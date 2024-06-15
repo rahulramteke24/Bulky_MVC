dbfca7de1a1c902e298a979952334d9682f6ef5e
rahul.ramteke <rahul24ramteke@gmail.com>
2024-06-15 15:45:16 +0530

    Razer Pages CRUD Operation with Edit & Delete

---
+++ BulkyWebRazor/Pages/Categories/Edit.cshtml.cs
@@ -0,0 +1,37 @@
+using BulkyWebRazor.Data;
+using BulkyWebRazor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWebRazor.Pages.Categories
+{
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public EditModel(ApplicationDbContext db)
+        {
+                _db = db;
+        }
+        [BindProperty]
+        public Category Category { get; set; }
+        public void OnGet(int? id)
+        {
+            if (id != null && id != 0)
+            {
+                Category = _db.Categories.Find(id);
+            }
+        }
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(Category);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully.";
+                return RedirectToPage("Index");
+            }
+            return Page();
+        }
+
+    }
+}
